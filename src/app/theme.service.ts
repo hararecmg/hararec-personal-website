8448ff43f94cf67c6abc0b76826a626023bf0761
Hararec MG <hararecmedina@gmail.com>
2023-02-02 16:30:48 -0600

    chore: se agrega ThemeService para temas visuales

---
+++ src/app/theme.service.ts
@@ -0,0 +1,34 @@
+import { DOCUMENT } from '@angular/common';
+import { Injectable, Inject, Renderer2, RendererFactory2 } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+
+  private theme: string = '';
+  private renderer: Renderer2;
+
+  constructor(
+    private rendererFactory: RendererFactory2,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+    this.setTheme(localStorage.getItem('theme') || this.getBrowserTheme());
+  }
+
+  toggleTheme() {
+    this.setTheme(this.theme === 'dark' ? 'light' : 'dark');
+  }
+
+  private setTheme(theme: string) {
+    this.theme = theme;
+    this.renderer.setAttribute(this.document.body, 'data-theme', this.theme);
+    localStorage.setItem('theme', this.theme);
+  }
+
+  private getBrowserTheme(): string {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  }
+
+}
