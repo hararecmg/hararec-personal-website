307f276c1962d23c137737be23dcab52b067d4d6
Hararec MG <hararecmedina@gmail.com>
2023-02-06 17:03:08 -0600

    chore: se modifica el servicio DeviceService para devolver un objeto Device

--- src/app/shared/services/device.service.ts
+++ src/app/shared/services/device.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Inject, Renderer2, RendererFactory2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable, map, shareReplay } from 'rxjs';
+import { Observable, map, shareReplay, BehaviorSubject } from 'rxjs';
+import { Device } from '../interfaces/device';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,13 @@ export class DeviceService {
   private isTablet$: Observable<boolean>;
   private isWeb$: Observable<boolean>;
   private renderer: Renderer2;
+  private _device: Device = {
+    isHandset: false,
+    isTablet: false,
+    isWeb: false
+  }
+  private deviceSubject = new BehaviorSubject<Device>(this._device);
+  device$ = this.deviceSubject.asObservable();
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -40,12 +48,24 @@ export class DeviceService {
   detectDeviceType() {
     this.isHandset$.subscribe(event => {
       this.renderer.setAttribute(this.document.documentElement, 'data-device', 'mobile');
+      this.device = {...this.device, isHandset: event };
     });
     this.isTablet$.subscribe(event => {
       this.renderer.setAttribute(this.document.documentElement, 'data-device', 'tablet');
+      this.device = {...this.device, isTablet: event };
     });
     this.isWeb$.subscribe(event => {
       this.renderer.setAttribute(this.document.documentElement, 'data-device', 'web');
+      this.device = {...this.device, isWeb: event };
     });
+  }
+
+  get device(): Device {
+    return this._device;
+  }
+
+  set device(device: Device) {
+    this._device = device;
+    this.deviceSubject.next(this._device);
   }
 }
