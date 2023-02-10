4f3dad86a6dfa2c20c62a1f5d739a5c8f8b76c26
Hararec MG <hararecmedina@gmail.com>
2023-02-09 23:58:19 -0600

    chore: se agregan locales en AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,36 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEn from '@angular/common/locales/en';
+import localeFr from '@angular/common/locales/fr';
+import localeZh from '@angular/common/locales/zh';
+import localeHi from '@angular/common/locales/hi';
+import localeAr from '@angular/common/locales/ar';
+import localeRu from '@angular/common/locales/ru';
+import localePt from '@angular/common/locales/pt';
+import localeDe from '@angular/common/locales/de';
+
+// Español
+registerLocaleData(localeEs);
+// Inglés
+registerLocaleData(localeEn);
+// Francés
+registerLocaleData(localeFr);
+// Chino
+registerLocaleData(localeZh);
+// Hindi
+registerLocaleData(localeHi);
+// Árabe
+registerLocaleData(localeAr);
+// Ruso
+registerLocaleData(localeRu);
+// Portugués
+registerLocaleData(localePt);
+// Alemán
+registerLocaleData(localeDe);
+
 
 @NgModule({
   declarations: [
