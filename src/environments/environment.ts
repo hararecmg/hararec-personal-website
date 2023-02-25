8e366db3af0840459ed10c915f772cf4ef35331b
Hararec MG <hararecmedina@gmail.com>
2023-02-25 02:06:43 -0600

    chore: se crea y se configura la carpeta environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    openAiApiKey: process.env.NG_OPENAI_API_KEY,
+    pexelsApiKey: process.env.NG_PEXELS_API_KEY,
+    pexelsBaseUrl: 'https://api.pexels.com',
+    openAiBaseUrl: 'https://api.openai.com',
+};
