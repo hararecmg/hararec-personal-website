34e31bc1bc1fa43d07ad3459415e82bbe67a5a0d
Hararec MG <hararecmedina@gmail.com>
2023-02-25 18:38:28 -0600

    chore: se agrega localStorege en PexelService y OpenAiService

--- src/app/shared/services/global/open-ai.service.ts
+++ src/app/shared/services/global/open-ai.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 import { OpenAIRequest, OpenAIResponse } from '../../interfaces/open-ai';
 import { environment } from '../../../../environments/environment.development';
 
@@ -12,12 +12,24 @@ export class OpenAiService {
   constructor(private http: HttpClient) { }
 
   getCompletions(body: OpenAIRequest): Observable<OpenAIResponse> {
-    return this.http.post<OpenAIResponse>(
-      `${environment.openAiBaseUrl}/v1/completions`,
-      JSON.stringify(body)
-    ).pipe(
-      map(this.mapOpenAIResponse)
-    );
+
+    let localData = 'openAi?';
+
+    Object.entries(body).forEach(item => {
+      if (item[0]) {
+        localData += `${item[0]}=${item[1]}&`;
+      }
+    })
+
+    return localStorage.getItem(localData)
+      ? of<OpenAIResponse>(JSON.parse(localStorage.getItem(localData) || ''))
+      : this.http.post<OpenAIResponse>(
+        `${environment.openAiBaseUrl}/v1/completions`,
+        JSON.stringify(body)
+      ).pipe(
+        map(this.mapOpenAIResponse),
+        tap(resp => localStorage.setItem(localData, JSON.stringify(resp)))
+      );
   }
 
   private mapOpenAIResponse(response: OpenAIResponse): OpenAIResponse {
--- src/app/shared/services/global/pexel.service.ts
+++ src/app/shared/services/global/pexel.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 import { PexelRequest, PexelResponse } from '../../interfaces/pexel';
 import { environment } from '../../../../environments/environment.development';
 
@@ -14,19 +14,24 @@ export class PexelService {
   searchPhotos(pexels: PexelRequest): Observable<PexelResponse> {
 
     let params = new HttpParams();
+    let localData = 'pexel?';
 
     Object.entries(pexels).forEach(item => {
       if (item[0]) {
         params = params.set(item[0], item[1]);
+        localData += `${item[0]}=${item[1]}&`;
       }
     })
 
-    return this.http.get<PexelResponse>(
-      `${environment.pexelBaseUrl}/v1/search/`,
-      { params }
-    ).pipe(
-      map(this.mapPexelResponse)
-    );
+    return localStorage.getItem(localData)
+      ? of<PexelResponse>(JSON.parse(localStorage.getItem(localData) || ''))
+      : this.http.get<PexelResponse>(
+        `${environment.pexelBaseUrl}/v1/search/`,
+        { params }
+      ).pipe(
+        map(this.mapPexelResponse),
+        tap(resp => localStorage.setItem(localData, JSON.stringify(resp)))
+      );
   }
 
   private mapPexelResponse(response: PexelResponse): PexelResponse {
