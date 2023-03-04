3d9ec013b36b6bda67c2541a2f6069a89169b965
Hararec MG <hararecmedina@gmail.com>
2023-03-04 16:47:08 -0600

    chore: se modifica el sistema de rutas a lazyload

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'home' },
+    children: [
+      // {
+      //   path: 'login',
+      //   component: LoginComponent
+      // },
+      // {
+      //   path: 'registro',
+      //   component: RegistroComponent
+      // },
+      // {
+      //   path: '**',
+      //   redirectTo: 'login'
+      // }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
+import { PrimengModule } from '../primeng/primeng.module';
 
 
 @NgModule({
@@ -10,10 +12,9 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ], 
-  exports: [
-    HomeComponent,
+    HomeRoutingModule,
+    SharedModule,
+    PrimengModule,
   ]
 })
 export class HomeModule { }
