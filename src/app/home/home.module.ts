54ce9b0f021d15d502ef3b97b69d04b321113b19
Hararec MG <hararecmedina@gmail.com>
2023-03-24 00:02:04 -0600

    chore: se agrega ScrollBottomComponent

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 import { PrimengModule } from '../primeng/primeng.module';
 import { ScrollingParallaxComponent } from './components/scrolling-parallax/scrolling-parallax.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ScrollBottomComponent } from './components/scroll-bottom/scroll-bottom.component';
 import { PersonalPhotographyComponent } from './components/personal-photography/personal-photography.component';
 
 
@@ -14,6 +15,7 @@ import { PersonalPhotographyComponent } from './components/personal-photography/
     HomeComponent,
     ScrollingParallaxComponent,
     WelcomeComponent,
+    ScrollBottomComponent,
     PersonalPhotographyComponent
   ],
   imports: [
