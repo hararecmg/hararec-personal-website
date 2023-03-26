727a99532aa48f311ceba8a6c2afffa4872b4fb4
Hararec MG <hararecmedina@gmail.com>
2023-03-26 00:17:48 -0600

    chore: se agrega funcionalidad de cambio de tema en ThemeButtonComponent

--- src/app/shared/components/theme-button/theme-button.component.ts
+++ src/app/shared/components/theme-button/theme-button.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { ThemeService } from './../../services/global/theme.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-theme-button',
   templateUrl: './theme-button.component.html',
   styleUrls: ['./theme-button.component.scss']
 })
-export class ThemeButtonComponent {
+export class ThemeButtonComponent implements OnInit {
+
+  isChecked: boolean = false;
+  themeForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private themeService: ThemeService,
+  ) { }
+
+  ngOnInit() {
+    this.isChecked = this.themeService.currentTheme !== 'dark'
+    this.themeForm = this.fb.group({
+      checked: this.isChecked,
+    });
+  }
+
+  changeTheme() {
+    this.isChecked = this.themeForm.controls['checked'].value
+    this.themeService.toggleTheme(); 
+  }
+
+   get themeName(): string {
+    return this.isChecked ? 'light' : 'dark';
+   }
 
 }
