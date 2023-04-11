a5feb269efefc69dce583397ae3dc3748bc74ad2
Hararec MG <hararecmedina@gmail.com>
2023-04-11 02:29:53 -0600

    chore: se modifican items y se agrega método buscar ruta en TopMenuComponent

--- src/app/shared/components/top-menu/top-menu.component.ts
+++ src/app/shared/components/top-menu/top-menu.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Renderer2, RendererFactory2, Inject, AfterViewChecked, AfterViewInit, HostListener } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 import { DeviceService } from 'src/app/shared/services/global/device.service';
-import { MenuItem, TreeNode } from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 import { Device } from '../../interfaces/device';
 
 @Component({
@@ -11,11 +12,11 @@ import { Device } from '../../interfaces/device';
 })
 export class TopMenuComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
-  items: MenuItem[] = [];
+  menuItems: MenuItem[] = [];
   themeName: string = '';
   value: string = '';
   val: number = 4;
-  files!: TreeNode<string>[];
+  // files!: TreeNode<string>[];
   isActive: boolean = false;
   isScrolled: boolean = false;
   elements!: NodeListOf<Element>;
@@ -26,6 +27,7 @@ export class TopMenuComponent implements OnInit, AfterViewInit, AfterViewChecked
   constructor(
     private rendererFactory: RendererFactory2,
     private device: DeviceService,
+    private router: Router,
     @Inject(DOCUMENT) private document: Document
   ) {
     this.renderer = this.rendererFactory.createRenderer(null, null);
@@ -42,111 +44,7 @@ export class TopMenuComponent implements OnInit, AfterViewInit, AfterViewChecked
 
   ngOnInit(): void {
     this.userDevice = this.device.device;
-    this.files = [
-      {
-        label: 'Documents',
-        data: 'Documents Folder',
-        expandedIcon: 'bi bi-chat-right-heart',
-        collapsedIcon: 'bi bi-chat-square-dots',
-        children: [
-          {
-            label: 'Work',
-            data: 'Work Folder',
-            expandedIcon: 'bi bi-chevron-right',
-            collapsedIcon: 'bi bi-chevron-down',
-            children: [
-              {
-                label: 'Expenses.doc',
-                icon: 'bi bi-clipboard',
-                data: 'Expenses Document'
-              },
-              {
-                label: 'Resume.doc',
-                icon: 'bi bi-clipboard-heart',
-                data: 'Resume Document'
-              }
-            ]
-          },
-          {
-            label: 'Home',
-            data: 'Home Folder',
-            expandedIcon: 'bi bi-clock',
-            collapsedIcon: 'bi bi-cloud-arrow-up',
-            children: [
-              {
-                label: 'Invoices.txt',
-                icon: 'pi pi-file',
-                data: 'Invoices for this month'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        label: 'Pictures',
-        data: 'Pictures Folder',
-        expandedIcon: 'pi pi-folder-open',
-        collapsedIcon: 'pi pi-folder',
-        children: [
-          {
-            label: 'barcelona.jpg',
-            icon: 'pi pi-image',
-            data: 'Barcelona Photo'
-          },
-          {
-            label: 'logo.jpg',
-            icon: 'pi pi-file',
-            data: 'PrimeFaces Logo'
-          },
-          {
-            label: 'primeui.png',
-            icon: 'pi pi-image',
-            data: 'PrimeUI Logo'
-          }
-        ]
-      },
-      {
-        label: 'Movies',
-        data: 'Movies Folder',
-        expandedIcon: 'pi pi-folder-open',
-        collapsedIcon: 'pi pi-folder',
-        children: [
-          {
-            label: 'Al Pacino',
-            data: 'Pacino Movies',
-            children: [
-              {
-                label: 'Scarface',
-                icon: 'pi pi-video',
-                data: 'Scarface Movie'
-              },
-              {
-                label: 'Serpico',
-                icon: 'pi pi-file-video',
-                data: 'Serpico Movie'
-              }
-            ]
-          },
-          {
-            label: 'Robert De Niro',
-            data: 'De Niro Movies',
-            children: [
-              {
-                label: 'Goodfellas',
-                icon: 'pi pi-video',
-                data: 'Goodfellas Movie'
-              },
-              {
-                label: 'Untouchables',
-                icon: 'pi pi-video',
-                data: 'Untouchables Movie'
-              }
-            ]
-          }
-        ]
-      }
-    ];
-    this.items = [
+    this.menuItems = [
       {
         label: 'Acerca de mi',
         icon: 'bi bi-person-circle',
@@ -157,11 +55,13 @@ export class TopMenuComponent implements OnInit, AfterViewInit, AfterViewChecked
             items: [
               {
                 label: 'Frontend',
-                icon: 'bi bi-window-fullscreen'
+                icon: 'bi bi-window-fullscreen',
+                command: () => this.findRoute('about', 'developer', 'frontend')
               },
               {
                 label: 'Backend',
-                icon: 'bi bi-pc-display'
+                icon: 'bi bi-pc-display',
+                command: () => this.findRoute('about', 'developer', 'backend')
               },
 
             ]
@@ -173,10 +73,12 @@ export class TopMenuComponent implements OnInit, AfterViewInit, AfterViewChecked
               {
                 label: 'Docente',
                 icon: 'bi bi-journal-bookmark',
+                command: () => this.findRoute('about', 'mathematician', 'teacher')
               },
               {
                 label: 'Investigador',
                 icon: 'bi bi-clipboard-heart-fill',
+                command: () => this.findRoute('about', 'mathematician', 'researcher')
               },
             ]
           }
@@ -185,6 +87,23 @@ export class TopMenuComponent implements OnInit, AfterViewInit, AfterViewChecked
       {
         label: 'Portafolio',
         icon: 'bi bi-pc-display-horizontal',
+        items: [
+          {
+            label: 'Desarrollador Frontend',
+            icon: 'bi bi-window-fullscreen',
+            command: () => this.findRoute('portfolio', 'frontend')
+          },
+          {
+            label: 'Desarrollador Backend',
+            icon: 'bi bi-pc-display',
+            command: () => this.findRoute('portfolio', 'backend')
+          },
+          {
+            label: 'Desarrollador Móvil',
+            icon: 'bi bi-phone',
+            command: () => this.findRoute('portfolio', 'mobile')
+          },
+        ]
       },
       {
         label: 'Blog',
@@ -226,4 +145,115 @@ export class TopMenuComponent implements OnInit, AfterViewInit, AfterViewChecked
       }, seconds * 1000);
   }
 
+  findRoute(base: string, ...params: string[]) {
+
+    this.router.navigate([`/${base}`, ...params])
+      .then(() => console.log(this.router.url.split(/\\|\//)[1]))
+      .catch(() => this.router.navigate(['']));
+  }
+
 }
+        // this.files = [
+        //   {
+        //     label: 'Documents',
+        //     data: 'Documents Folder',
+        //     expandedIcon: 'bi bi-chat-right-heart',
+        //     collapsedIcon: 'bi bi-chat-square-dots',
+        //     children: [
+        //       {
+        //         label: 'Work',
+        //         data: 'Work Folder',
+        //         expandedIcon: 'bi bi-chevron-right',
+        //         collapsedIcon: 'bi bi-chevron-down',
+        //         children: [
+        //           {
+        //             label: 'Expenses.doc',
+        //             icon: 'bi bi-clipboard',
+        //             data: 'Expenses Document'
+        //           },
+        //           {
+        //             label: 'Resume.doc',
+        //             icon: 'bi bi-clipboard-heart',
+        //             data: 'Resume Document'
+        //           }
+        //         ]
+        //       },
+        //       {
+        //         label: 'Home',
+        //         data: 'Home Folder',
+        //         expandedIcon: 'bi bi-clock',
+        //         collapsedIcon: 'bi bi-cloud-arrow-up',
+        //         children: [
+        //           {
+        //             label: 'Invoices.txt',
+        //             icon: 'pi pi-file',
+        //             data: 'Invoices for this month'
+        //           }
+        //         ]
+        //       }
+        //     ]
+        //   },
+        //   {
+        //     label: 'Pictures',
+        //     data: 'Pictures Folder',
+        //     expandedIcon: 'pi pi-folder-open',
+        //     collapsedIcon: 'pi pi-folder',
+        //     children: [
+        //       {
+        //         label: 'barcelona.jpg',
+        //         icon: 'pi pi-image',
+        //         data: 'Barcelona Photo'
+        //       },
+        //       {
+        //         label: 'logo.jpg',
+        //         icon: 'pi pi-file',
+        //         data: 'PrimeFaces Logo'
+        //       },
+        //       {
+        //         label: 'primeui.png',
+        //         icon: 'pi pi-image',
+        //         data: 'PrimeUI Logo'
+        //       }
+        //     ]
+        //   },
+        //   {
+        //     label: 'Movies',
+        //     data: 'Movies Folder',
+        //     expandedIcon: 'pi pi-folder-open',
+        //     collapsedIcon: 'pi pi-folder',
+        //     children: [
+        //       {
+        //         label: 'Al Pacino',
+        //         data: 'Pacino Movies',
+        //         children: [
+        //           {
+        //             label: 'Scarface',
+        //             icon: 'pi pi-video',
+        //             data: 'Scarface Movie'
+        //           },
+        //           {
+        //             label: 'Serpico',
+        //             icon: 'pi pi-file-video',
+        //             data: 'Serpico Movie'
+        //           }
+        //         ]
+        //       },
+        //       {
+        //         label: 'Robert De Niro',
+        //         data: 'De Niro Movies',
+        //         children: [
+        //           {
+        //             label: 'Goodfellas',
+        //             icon: 'pi pi-video',
+        //             data: 'Goodfellas Movie'
+        //           },
+        //           {
+        //             label: 'Untouchables',
+        //             icon: 'pi pi-video',
+        //             data: 'Untouchables Movie'
+        //           }
+        //         ]
+        //       }
+        //     ]
+        //   }
+        // ];
