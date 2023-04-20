24ae7a19ea3d76366e8412007d507f993ca02c3f
Hararec MG <hararecmedina@gmail.com>
2023-04-20 17:30:55 -0600

    chore: se agregan rutas secundarias por parámatros en los módulos blog, contact y portfolio

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: '',
     component: PageUnderDevelopmentComponent,
     data: { title: 'blog' }
+  },
+  {
+    path: ':blog',
+    component: PageUnderDevelopmentComponent,
   }
 ];
 
