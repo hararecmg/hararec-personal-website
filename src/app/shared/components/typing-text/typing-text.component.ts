ebda64d329c6c5b4e246c71fff18e140402485a1
Hararec MG <hararecmedina@gmail.com>
2023-04-23 15:59:55 -0600

    chore: se personalizan los estilos para cada dispositivo en TypingTextComponent

--- src/app/shared/components/typing-text/typing-text.component.ts
+++ src/app/shared/components/typing-text/typing-text.component.ts
@@ -5,6 +5,8 @@ import { gsap } from 'gsap';
 import { TextPlugin } from 'gsap/TextPlugin';
 import { OpenAiService } from '../../services/global/open-ai.service';
 import { OpenAIResponse } from '../../interfaces/open-ai';
+import { DeviceService } from '../../services/global/device.service';
+import { Device } from '../../interfaces/device';
 
 @Component({
   selector: 'app-typing-text',
@@ -15,19 +17,24 @@ export class TypingTextComponent implements OnInit, AfterViewInit {
 
   private _openAiResponse!: OpenAIResponse;
   words: string = 'Hello world!';
+  isLoading: boolean = true;
+  userDevice!: Device;
 
   constructor(
     private openAi: OpenAiService,
     private router: Router,
+    private device: DeviceService,
   ) {}
   
   ngOnInit(): void {
 
     const urlArray = this.router.url.split('/');
-
+    
     this.words = urlArray
     .filter((valor, indice) => urlArray.indexOf(valor) === indice)
     .slice(1).join(' ');
+
+    this.userDevice = this.device.device;
 
     this.openAi.getModerateCompletions({
       model: 'text-davinci-003',
@@ -37,9 +44,12 @@ export class TypingTextComponent implements OnInit, AfterViewInit {
       frequency_penalty: 0,
       presence_penalty: 0,
       top_p: 1,
-    }, true)
+    }, false)
     .pipe(take(1))
-    .subscribe(resp => this._openAiResponse = resp);
+    .subscribe(resp => {
+      this.isLoading = false;
+      this._openAiResponse = resp
+    });
     
     gsap.registerPlugin(TextPlugin);
   }
@@ -58,13 +68,13 @@ export class TypingTextComponent implements OnInit, AfterViewInit {
     });
     tl.to('.box', {
       duration: 1,
-      width: '18vw',
+      width: this.cssValues['box-width'],
       delay: 0.2,
       ease: 'power4.inOut',
     })
     .from('.hi', {
       duration: 1,
-      y: '7vw',
+      y: this.cssValues['hi-y'],
       ease: 'power3.inOut',
     });
     wordTl.to('.text', {
@@ -80,18 +90,62 @@ export class TypingTextComponent implements OnInit, AfterViewInit {
     return `Reflexiona sobre la naturaleza de: "${words}" y escribe una frase filosófica sobre ello:`; 
   }
 
-  // markAasFavorite() {
-  //   this.ref.close(this.openAiResponse);
-  // }
+  formatUnixTime(unixTime: number): string {
+    const date = new Date(unixTime * 1000); // se convierte a milisegundos
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString().slice(-2);
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+  
+    return `${day}/${month}/${year} & ${hours}:${minutes}:${seconds}`;
+  }
 
+  get cssValues(): {[key: string]: string} {
+    return {
+      'hi-font-size': this.userDevice.isHandset
+        ? '3vw'
+        : this.userDevice.isTablet
+          ? '1.5vw'
+          : this.userDevice.isWeb || this.userDevice.isXLarge
+            ? '1.2vw'
+            : '1.2vw',
+      'cursor-font-size': this.userDevice.isHandset
+        ? '4.2vw'
+        : this.userDevice.isTablet
+          ? '2.2vw'
+          : this.userDevice.isWeb || this.userDevice.isXLarge
+            ? '1.8vw'
+            : '1.8vw',
+      'hi-y': this.userDevice.isHandset
+        ? '12vw'
+        : this.userDevice.isTablet
+          ? '10vw'
+          : this.userDevice.isWeb || this.userDevice.isXLarge
+            ? '5vw'
+            : '5vw',
+      'box-width': this.userDevice.isHandset
+        ? '100%'
+        : this.userDevice.isTablet
+          ? '62%'
+          : this.userDevice.isWeb
+            ? '70%'
+            : '100%',
+      'quote-color': this.isLoading
+        ? 'transparent'
+        : '',
+    }
+  }
+  
   get openAiResponse(): {[key: string]: string} {
 
-    const date = this._openAiResponse.created;
+    const date = this.formatUnixTime(this._openAiResponse.created);
     const model = this._openAiResponse.model;
 
     return {
       'text': this._openAiResponse.choices[0].text || 'hello world',
-      'quote': `Texto creado por ${model} (inteligencia artificial) en ${date}` || 'texto creado',
+      'quote': `Texto creado por '${model}' - ${date}` || '',
       'id': this._openAiResponse.id,
     }
   }
